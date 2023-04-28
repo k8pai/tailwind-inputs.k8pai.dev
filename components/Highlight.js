a886dc1ef1eedd9accc8211e6d9af6094791a628
k8pai <thek8pai@gmail.com>
2023-04-28 23:48:56 +0530

    updated with color change of text

--- components/Highlight.js
+++ components/Highlight.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Highlight = ({ type, title, ...rest }) => {
 	// if (type)
 	return (
-		<span className="text-white italic" {...rest}>
+		<span className="text-highlight italic" {...rest}>
 			{title}
 		</span>
 	);
